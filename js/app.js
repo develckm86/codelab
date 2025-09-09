904dfeaaab8e13a24eabf977ef6948c3f106604d
kyoungmin <develckm86@gmail.com>
2025-09-09 16:40:21 +0900

    first commit

---
+++ js/app.js
@@ -0,0 +1,11 @@
+
+async function load(id, file) {
+  const res = await fetch(file);
+  document.getElementById(id).innerHTML = await res.text();
+}
+load("header", "/template/header.html");
+load("footer", "/template/footer.html");
+load("aside", "/template/aside.html");
+
+const offcanvasElementList = document.querySelectorAll('.offcanvas')
+const offcanvasList = [...offcanvasElementList].map(offcanvasEl => new bootstrap.Offcanvas(offcanvasEl))
