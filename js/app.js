f4b76008eca83ad40ff70fea249374c82a61658a
kyoungmin <develckm86@gmail.com>
2025-09-09 18:02:07 +0900

    경로 수정 및 제클 삭제

--- js/app.js
+++ js/app.js
@@ -3,9 +3,9 @@ async function load(id, file) {
   const res = await fetch(file);
   document.getElementById(id).innerHTML = await res.text();
 }
-load("header", "/template/header.html");
-load("footer", "/template/footer.html");
-load("aside", "/template/aside.html");
+load("header", "./template/header.html");
+load("footer", "./template/footer.html");
+load("aside", "./template/aside.html");
 
 const offcanvasElementList = document.querySelectorAll('.offcanvas')
 const offcanvasList = [...offcanvasElementList].map(offcanvasEl => new bootstrap.Offcanvas(offcanvasEl))
